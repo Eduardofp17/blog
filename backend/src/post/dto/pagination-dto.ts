4db5bbf4811d7df542d2f79a0ed7229509371456
Eduardo F. Pinheiro <edardofernandespinheiro@gmail.com>
2025-02-02 13:08:05 -0300

    (fix): fixing IsOptional on dto fields

--- backend/src/post/dto/pagination-dto.ts
+++ backend/src/post/dto/pagination-dto.ts
@@ -1,14 +1,12 @@
-import { IsOptional, IsPositive, Min } from 'class-validator';
+import { IsPositive, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class PaginationDto {
-  @IsOptional()
   @Type(() => Number)
   @IsPositive()
   @Min(1)
   page?: number;
 
-  @IsOptional()
   @Type(() => Number)
   @IsPositive()
   @Min(1)
