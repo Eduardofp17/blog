a71aec48b62613551f4132b4320bc713aa9dfa16
Eduardo F. Pinheiro <edardofernandespinheiro@gmail.com>
2025-02-14 17:18:29 -0300

    (fix): adding the frontend domain to the origin whitelist

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'eduardofp.blog', 'www.eduardofp.blog'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
   });
